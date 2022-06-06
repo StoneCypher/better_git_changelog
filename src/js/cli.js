fecb3a62f47c439ef8ecb8d3a6eb87e98ea91aa7
John Haugeland <stonecypher@gmail.com>
2022-06-06 08:38:32 -0700

    long and short mechanics

--- src/js/cli.js
+++ src/js/cli.js
@@ -4,6 +4,8 @@ const api = require('./index.js');
 
 const { program } = require('commander');
 
+
+
 program
   .option('-l, --long-form',             'Include all commits in the changelog')
   .option('-s, --short-form',            'Include only the current and previous commit in the changelog')
@@ -19,5 +21,31 @@ const opts = program.opts();
 
 
 console.log('Generating CHANGELOG.md...');
-api.write_md();
+
+let long  = false,
+    short = false,
+    fn    = opts.filename,
+    fn2   = opts.bothForms;
+
+if (opts.longForm)  { long  = true; }
+if (opts.shortForm) { short = true; }
+if (opts.bothForms) { short = true; long = true; }
+
+if ( (!(long)) && (!(short)) ) { long = true; }
+
+
+
+if (long && short) {
+  api.write_short_md(fn);
+  api.write_long_md(fn2);
+
+} else if (long) {
+  api.write_long_md(fn);
+
+} else if (short) {
+  api.write_short_md(fn);
+}
+
+
+
 console.log('  finished!');
